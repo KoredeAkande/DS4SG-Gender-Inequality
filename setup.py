d73e2402ee70c4510856bb4a0a6f1a49121b5d24
Korede Akande <55578569+KoredeAkande@users.noreply.github.com>
2022-01-25 15:10:51 -0800

    Added Cookiecutter Data Science structure
    Added Cookiecutter Data Science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project, we determine if there is a correlation between road quality and income, using  a deep learning model for road image classification',
+    author='Team KD',
+    license='MIT',
+)
